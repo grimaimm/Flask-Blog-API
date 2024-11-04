9c9f8ad63edac90d884ef05b65d3d155c759c58f
grimaim <muhammadrahim.mr196@gmail.com>
2024-11-04 23:25:59 +0700

    chore: register blueprint

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from config import app, db
+from routes.AuthorRoute import AuthorRoute
+from routes.CategoryRoute import CategoryRoute
+from routes.CommentRoute import CommentRoute
+from routes.PostRoute import PostRoute
+
+# Register Blueprints
+app.register_blueprint(AuthorRoute)
+app.register_blueprint(CategoryRoute)
+app.register_blueprint(CommentRoute)
+app.register_blueprint(PostRoute)
 
 if __name__ == "__main__":
     with app.app_context():
---
+++ index.py
@@ -0,0 +1,16 @@
+from config import app, db
+from routes.AuthorRoute import AuthorRoute
+from routes.CategoryRoute import CategoryRoute
+from routes.CommentRoute import CommentRoute
+from routes.PostRoute import PostRoute
+
+# Register Blueprints
+app.register_blueprint(AuthorRoute)
+app.register_blueprint(CategoryRoute)
+app.register_blueprint(CommentRoute)
+app.register_blueprint(PostRoute)
+
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+#     app.run(port=5003, debug=True)
